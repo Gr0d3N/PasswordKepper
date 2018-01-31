b73bb1d4329feea97ed4fc62b386b6a5375efe63
Mina Habib <minahabi@amazon.com>
2018-01-31 09:09:19 -0800

    Modify the Service class structure
    Split the class into 2 classes, service and password.
    Add Constructor that will create a class object through data based form
    the database.

--- src/Service.java
+++ src/Service.java
@@ -1,6 +1,9 @@
+import java.math.BigInteger;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.NoSuchAlgorithmException;
 import java.time.Instant;
+import java.util.Random;
 
 public class Service {
 
@@ -8,13 +11,11 @@ public class Service {
     private String username;
     private String email;
     private String serviceName;
-    private int passwordLength;
-    private Instant lastUpdated;
-    private boolean containSymbols;
-    private boolean containNumbers;
+    private Password password;
 
     /**
      * Constructor for the Service class
+     * This constructor is used to create a new class object by passing all fields
      * @param serviceURL
      * @param username
      * @param email
@@ -24,16 +25,36 @@ public class Service {
      * @param containNumbers
      */
     public Service(String serviceURL, String username, String email, String serviceName,
-                   int passwordLength, boolean containSymbols, boolean containNumbers) throws MalformedURLException {
+                   int passwordLength, boolean containSymbols, boolean containNumbers,
+                   String masterPassword) throws MalformedURLException, NoSuchAlgorithmException {
         this.serviceURL = new URL(serviceURL);
         this.username = username;
         this.email = email;
         this.serviceName = serviceName;
-        this.passwordLength = passwordLength;
-        this.lastUpdated = Instant.now();
-        this.containSymbols = containSymbols;
-        this.containNumbers = containNumbers;
+        password = new Password(passwordLength, containSymbols, containNumbers, serviceName, masterPassword);
+    }
 
+    /**
+     * Constructor for the Service class
+     * This constructor is used to create a new service object by pulling the fields from a database
+     * @param serviceURL
+     * @param username
+     * @param email
+     * @param serviceName
+     * @param passwordLength
+     * @param containSymbols
+     * @param containNumbers
+     * @param lastUpdated
+     */
+    public Service(String serviceURL, String username, String email, String serviceName,
+                   int passwordLength, boolean containSymbols, boolean containNumbers,
+                   String masterPassword, Instant lastUpdated) throws MalformedURLException, NoSuchAlgorithmException {
+        this.serviceURL = new URL(serviceURL);
+        this.username = username;
+        this.email = email;
+        this.serviceName = serviceName;
+        password = new Password(passwordLength, containSymbols, containNumbers, lastUpdated,
+                serviceName, masterPassword);
     }
 
     /**
@@ -45,7 +66,15 @@ public class Service {
     }
 
     /**
-     * The getter method for the user name
+     * The setter method for the service URL
+     * @param serviceURL
+     */
+    public void SetServiceURL(URL serviceURL) {
+        this.serviceURL = serviceURL;
+    }
+
+    /**
+     * The getter method for the username
      * @return String
      */
     public String getUsername() {
@@ -53,11 +82,11 @@ public class Service {
     }
 
     /**
-     * The getter method for the last updated date
-     * @return Instant
+     * The setter method for the username
+     * @param username
      */
-    public Instant getLastUpdated() {
-        return lastUpdated;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     /**
@@ -69,6 +98,14 @@ public class Service {
     }
 
     /**
+     * The setter method for the email
+     * @param email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
      * The getter method for the service name
      * @return String
      */
@@ -77,33 +114,44 @@ public class Service {
     }
 
     /**
-     * The getter method for the password length
-     * @return int
+     * The setter method for the service name
+     * @param serviceName
      */
-    public int getPasswordLength() {
-        return passwordLength;
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
     }
 
     /**
-     * The getter method for contains symbols
-     * @return boolean
+     * The getter method for the service password
+     * @return Password object
      */
-    public boolean isContainSymbols() {
-        return containSymbols;
+    public Password getPassword() {
+        return password;
     }
 
     /**
-     * The getter method for contain numbers
-     * @return boolean
+     * The setter method for the service password
+     * @param password
      */
-    public boolean isContainNumbers() {
-        return containNumbers;
+    public void setPassword(Password password) {
+        this.password = password;
     }
 
-    public static void main(String[] args) throws MalformedURLException {
-        Service test = new Service("https://facebook.com", "minahabib", "mina@minahabib.com",
-                "Facebook", 24, true, false);
-        System.out.println(test.getLastUpdated().getEpochSecond());
-    }
+    public static void main(String[] args) throws MalformedURLException, NoSuchAlgorithmException {
 
+        /*
+        Instant ins = Instant.ofEpochSecond(1517355280);
+        Service test = new Service ("https://facebook.com", "minahabib",
+                "mina@minahabib.com", "facebook", 24, true,
+                false, "raouffalse", ins);
+         */
+
+        Service test = new Service ("https://facebook.com", "minahabib",
+                "mina@minahabib.com", "facebook", 24, true,
+                false, "myMasterPassword");
+
+        Password pass = test.getPassword();
+        String finalPass = pass.getCredential();
+        System.out.println(finalPass);
+    }
 }
